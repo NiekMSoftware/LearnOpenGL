6269a22af4bb529fb33462b63a408446a35afd46
NiekMSoftware <niek+github@meletstudios.com>
2025-11-04 12:13:24 +0100

    Added a RectangleApp to the `Hello_Triangle` project

--- LearnOpenGL/hello_triangle/main.cpp
+++ LearnOpenGL/hello_triangle/main.cpp
@@ -65,6 +65,8 @@ protected:
 
     void OnShutdown() override {
         glDeleteProgram(shaderProgram);
+        glDeleteBuffers(1, &VBO);
+        glDeleteVertexArrays(1, &VAO);
     }
 
     void OnUpdate() override {  }
@@ -103,9 +105,131 @@ private:
     })";
 };
 
+class RectangleApp final : public App {
+public:
+    RectangleApp() = default;
+
+    explicit RectangleApp(const AppProperties &properties)
+        : App(properties), VBO(0), VAO(0), EBO(0), shaderProgram(0), vertexShader(0), fragmentShader(0) {}
+
+protected:
+    void OnInit() override {
+        // Generate buffers
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+
+        // Bind buffers
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+        // Set input
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(nullptr));
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+
+        // Create shaders
+        vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+        glCompileShader(vertexShader);
+
+        // Compile vertex shader
+        int success;
+        char infoLog[512];
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+        if (!success) {
+            glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+            std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+
+        fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+        glCompileShader(fragmentShader);
+        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+        if (!success) {
+            glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+            std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+
+        // Link program
+        shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+        glLinkProgram(shaderProgram);
+        glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+        if (!success) {
+            glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+            std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        }
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+    }
+
+    void OnShutdown() override {
+        glDeleteProgram(shaderProgram);
+        glDeleteBuffers(1, &VBO);
+        glDeleteBuffers(1, &EBO);
+        glDeleteVertexArrays(1, &VAO);
+    }
+
+    void OnUpdate() override {
+
+    }
+
+    void OnRender() override {
+        glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+    }
+
+private:
+    float vertices[12] = {
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+       -0.5f, -0.5f, 0.0f,  // bottom left
+       -0.5f,  0.5f, 0.0f   // top left
+   };
+    unsigned int indices[9] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    // buffer
+    unsigned int VBO, VAO, EBO;
+
+    // shader
+    unsigned int shaderProgram;
+    unsigned int vertexShader, fragmentShader;
+
+    const char* vertexShaderSource = R"(#version 330 core
+    layout (location = 0) in vec3 aPos;
+    void main() {
+        gl_Position = vec4(vec3(aPos), 1.0);
+    })";
+    const char* fragmentShaderSource = R"(#version 330 core
+    out vec4 FragColor;
+    void main() {
+        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    })";
+};
+
 int main() {
+    // change value to toggle between apps
+#if 0
     constexpr AppProperties props{ "Hello Triangle", 800, 600 };
     const std::unique_ptr<TriangleApp> triangleApp = std::make_unique<TriangleApp>(props);
     triangleApp->Run();
+#else
+    constexpr AppProperties props { "Hello Rectangle", 800, 600 };
+    const std::unique_ptr<RectangleApp> rectangleApp = std::make_unique<RectangleApp>(props);
+    rectangleApp->Run();
+#endif
+
     return 0;
 }
