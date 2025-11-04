cf7f0d423d07320d6df8111b8dfe120dcf7577cb
NiekMSoftware <niek+github@meletstudios.com>
2025-11-04 12:23:42 +0100

    Completed exercise 1 - Drawing 2 triangles next to each other

--- LearnOpenGL/hello_triangle/main.cpp
+++ LearnOpenGL/hello_triangle/main.cpp
@@ -219,16 +219,130 @@ private:
     })";
 };
 
+class ExerciseApp final : public App {
+public:
+    ExerciseApp() = default;
+
+    explicit ExerciseApp(const AppProperties &properties)
+        : App(properties), VBO(0), VAO(0), vertexShader(0), fragmentShader(0), shaderProgram(0) {
+
+    }
+
+void OnInit() override {
+        // Generate buffers
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(nullptr));
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+
+        // Create shaders
+        vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+        glCompileShader(vertexShader);
+
+        // Compile vertex shader
+        int success;
+        char infoLog[512];
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+        if (!success) {
+            glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+            std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+
+        fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+        glCompileShader(fragmentShader);
+        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+        if (!success) {
+            glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+            std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+
+        // Link program
+        shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+        glLinkProgram(shaderProgram);
+        glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+        if (!success) {
+            glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+            std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        }
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+    }
+
+    void OnShutdown() override {
+        glDeleteProgram(shaderProgram);
+        glDeleteBuffers(1, &VBO);
+        glDeleteVertexArrays(1, &VAO);
+    }
+
+    void OnUpdate() override {  }
+
+    void OnRender() override {
+        glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+    }
+
+private:
+    unsigned int VBO;
+    unsigned int VAO;
+
+    // shaders
+    unsigned int vertexShader, fragmentShader;
+    unsigned int shaderProgram;
+
+    // vertices for two triangles
+    float vertices[18] {
+        -0.9f, -0.5f, 0.0f,
+        -0.0f, -0.5f, 0.0f,
+        -0.45f, 0.5f, 0.0f,
+
+        0.0f, -0.5f, 0.0f,
+        0.9f, -0.5f, 0.0f,
+        0.45f, 0.5f, 0.0f
+    };
+
+    const char* vertexShaderSource = R"(#version 330 core
+    layout (location = 0) in vec3 aPos;
+    void main() {
+        gl_Position = vec4(vec3(aPos), 1.0);
+    })";
+    const char* fragmentShaderSource = R"(#version 330 core
+    out vec4 FragColor;
+    void main() {
+        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    })";
+};
+
 int main() {
-    // change value to toggle between apps
+    // change value to toggle between exercises or regular showcase
 #if 0
+    // change value to toggle between apps
+    #if 0
     constexpr AppProperties props{ "Hello Triangle", 800, 600 };
     const std::unique_ptr<TriangleApp> triangleApp = std::make_unique<TriangleApp>(props);
     triangleApp->Run();
-#else
+    #else
     constexpr AppProperties props { "Hello Rectangle", 800, 600 };
     const std::unique_ptr<RectangleApp> rectangleApp = std::make_unique<RectangleApp>(props);
     rectangleApp->Run();
+    #endif
+#else
+
+    constexpr AppProperties props { "Exercise", 800, 600 };
+    const std::unique_ptr<ExerciseApp> exerciseApp = std::make_unique<ExerciseApp>(props);
+    exerciseApp->Run();
+
 #endif
 
     return 0;
