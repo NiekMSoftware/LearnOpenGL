25499a7e5427be8852701e7691464f002078a45d
NiekMSoftware <niek+github@meletstudios.com>
2025-11-06 11:32:49 +0100

    More attributes

--- LearnOpenGL/shaders/main.cpp
+++ LearnOpenGL/shaders/main.cpp
@@ -20,8 +20,15 @@ protected:
         glBindVertexArray(VAO);
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(nullptr));
+
+        // position attribute
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), static_cast<void*>(nullptr));
         glEnableVertexAttribArray(0);
+
+        // color attribute
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindVertexArray(0);
 
@@ -86,10 +93,11 @@ protected:
     }
 
 private:
-    float vertices[9] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f,
+    float vertices[18] = {
+        // positions        // colors
+        0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+       -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+        0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top
     };
     unsigned int VAO, VBO;
 
@@ -98,14 +106,19 @@ private:
 
     const char* vertexShaderSource = R"(#version 330 core
     layout (location = 0) in vec3 aPos;
+    layout (location = 1) in vec3 aColor;
+
+    out vec3 ourColor;
+
     void main() {
-        gl_Position = vec4(vec3(aPos), 1.0);
+        gl_Position = vec4(aPos, 1.0);
+        ourColor = aColor;
     })";
     const char* fragmentShaderSource = R"(#version 330 core
     out vec4 FragColor;
-    uniform vec4 ourColor;
+    in vec3 ourColor;
     void main() {
-        FragColor = ourColor;
+        FragColor = vec4(ourColor, 1.0);
     })";
 };
 
